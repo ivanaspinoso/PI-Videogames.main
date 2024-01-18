40192a11ab7650cd06de9e5e9a3487473ad2eda7
ivanaspinoso <ivanaspinoso@MacBook-Air-de-Agustin.local>
2024-01-18 13:57:28 -0300

    ultimos cambios

--- client/PI-Videogames/src/Views/Landing/Landing.jsx
+++ client/PI-Videogames/src/Views/Landing/Landing.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import s from "./Landing.module.css"
 
 export const Landing = () => {
   return (
-    <div>Landing</div>
+    <div className={s.landing}>
+      <h1 className={s.title}>Videogames</h1>
+      <Link to="/">
+        <button className={s.eightbitBtn}>
+          Play!
+        </button>
+      </Link>
+
+
+
+    </div>
   )
 }
